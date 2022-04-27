3e1e54317c99216e43a5cd70e7b065e53e5832ad
zerolionsee <hekun6688@163.com>
2022-04-27 22:17:31 +0800

    多个.C文件，改tasks.json成功了

---
+++ hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    extern int g;
+    printf("g=%d\n",g);
+    return 0;
+    /* printf("hehe son!/n");
+    return 0; */
+    /* int num1,num2;
+    scanf("%d%d",&num1,&num2);
+    int sum=0;
+    sum=num1+num2;
+    printf("sum=%d\n",sum);
+    return 0; */
+    /* char ch='A';'
+    int  age=20;
+    float weight  =2;
+    double  c=3;
+    printf("i love u\n %c\n%d\n%f\n%lf\n",ch,age,weight,c);
+    printf("%d\n",sizeof(ch));
+    printf("%d\n",sizeof(age));
+    printf("%d\n",sizeof(weight));
+    printf("%d\n",sizeof(c));
+    return 0; */
+}
+
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int g=121;
+int test()
+{
+  printf("g--\n%d\n",g);
+}
